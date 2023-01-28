0bbb5ab542e7a02dcf70b54afb81fba8450eb245
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-01-28 01:51:15 -0500

    Refactor and search bar to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
-import Primary2Button from "../components/shared/Primary2Button";
-import { FaSearch } from "react-icons/fa";
-
+import SearchBar from "../components/Home/SearchBar";
 function Home() {
-  const handleClick = () => {
-    console.log("Button was clicked");
-  };
-
   return (
     <div className="">
       <div className="text-6xl text-light-1 font-bold">Find movies for you</div>
-      <Primary2Button name="Search" icon={<FaSearch />} onClick={handleClick} />
+      <SearchBar />
     </div>
   );
 }
