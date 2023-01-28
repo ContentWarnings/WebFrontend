4306c945091e9d2a97339306b1a585a83e633aa4
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-01-28 00:49:38 -0500

    Rename configurated colors and change files to match

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'background-black': '#222222',
-        'light-pink': '#7e57c2',
-        'dark-pink': '#4d2c91',
+        'primary-1': '#b085f5',
+        'primary-2': '#7e57c2',
+        'primary-3': '#4d2c91',
+        'secondary-1': '#63a4ff',
+        'secondary-2': '#1976d2',
+        'secondary-3': '#004ba0',
+        'light-1': '#ffffff',
+        'light-2': '#e0e0e0',
+        'light-3': '#b0b0b0',
+        'dark-1': '#313131',
+        'dark-2': '#1e1e1e',
+        'dark-3': '#000000',
       },
     },
   },
