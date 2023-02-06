d35e5a8d8edf50175c90f516c32b76753582dbb3
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-02-06 15:21:26 -0500

    Finish above carousel title and button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
 import SearchBar from "../components/Home/SearchBar";
+import Primary2Button from "../components/shared/Primary2Button";
 
 function Home() {
+  const handleClick = () => {
+    console.log("Button was clicked");
+  };
+
   return (
     <div className="relative px-6 h-screen">
       <div className="text-center">
@@ -11,6 +16,12 @@ function Home() {
         <div className="mx-auto max-w-2xl mt-20">
           <SearchBar />
         </div>
+      </div>
+      <div className="flex justify-between mt-20 px-10">
+        <h1 className="text-2xl text-light-1 font-bold">
+          Trending Movies For You
+        </h1>
+        <Primary2Button name="View More" handleClick={handleClick} />
       </div>
     </div>
   );
