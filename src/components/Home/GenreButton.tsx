2eaef03bc97e607d8dcdfe56f689d47d06a35f84
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-02-12 15:25:33 -0500

    Work on Genre Button title

--- src/components/Home/GenreButton.tsx
+++ src/components/Home/GenreButton.tsx
@@ -3,9 +3,9 @@ function GenreButton(props: any) {
   return (
     <button
       onClick={props.handleClick}
-      className={`transition ease-in-out delay-100 ${props.img} hover:border-light-1 text-light-1 py-1 px-2 rounded`}
+      className={`transition ease-in-out delay-100 ${props.img} hover:border-light-1  py-1 px-2 rounded`}
     >
-      <div className="items-center">
+      <div className="items-center text-light-1 text-3xl font-bold">
         <div className="px-1">{props.name}</div>
       </div>
     </button>
