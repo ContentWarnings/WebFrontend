87af07b275f5688f0516a946267c974f41362923
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-02-28 16:25:11 -0500

    Blur background of popups further

--- src/components/MovieEntry/AddContentWarning.tsx
+++ src/components/MovieEntry/AddContentWarning.tsx
@@ -27,7 +27,7 @@ function AddContentWarning() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
--- src/components/MovieEntry/ContentWarningButton.tsx
+++ src/components/MovieEntry/ContentWarningButton.tsx
@@ -81,7 +81,7 @@ function ContentWarningButton(props: any) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
