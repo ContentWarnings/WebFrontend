970276d680f290eabedacdeba0a03bfb4c29ae49
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-03-15 01:10:44 -0400

    Connect to local storage for values

--- src/components/Settings/CWInput.tsx
+++ src/components/Settings/CWInput.tsx
@@ -1,11 +1,15 @@
 // References
 // https://beta.reactjs.org/reference/react-dom/components/input
+// https://www.freecodecamp.org/news/how-to-remove-an-element-from-a-javascript-array-removing-a-specific-item-in-js/
+// https://gomakethings.com/how-to-update-localstorage-with-vanilla-javascript/
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Backend from "../../helpers/Backend";
 
 function CWInput(props: any) {
   const [message, setMessage] = useState("");
+  const [visibility, setVisibility] = useState("");
+
   const handleClick = () => {
     if (message === "") {
       async function fetchData() {
@@ -18,20 +22,48 @@ function CWInput(props: any) {
     } else setMessage("");
   };
 
+  useEffect(() => {
+    // Get the existing data
+    const existingList = localStorage.getItem("cw");
+
+    // If no existing data, create an array
+    // Otherwise, convert the localStorage string to an array
+    const cwList = existingList ? JSON.parse(existingList) : {};
+
+    setVisibility(cwList[`${props.name}`]);
+  }, [props.name]);
+
+  useEffect(() => {
+    // Get the existing data
+    const existingList = localStorage.getItem("cw");
+
+    // If no existing data, create an array
+    // Otherwise, convert the localStorage string to an array
+    let cwList = existingList ? JSON.parse(existingList) : {};
+
+    cwList[`${props.name}`] = visibility;
+
+    // Save back to localStorage
+    localStorage.setItem("cw", JSON.stringify(cwList));
+  }, [props.name, visibility]);
+
   return (
     <div className="border-t-4 border-dark-2 py-2 ">
       <div className="flex justify-between">
-        <div className="flex">
-          <h2 className="text-xl text-light-1">{props.name}</h2>
-          <button
-            type="button"
-            onClick={() => handleClick()}
-            className={
-              "ml-2 rounded-full border-primary-2 bg-primary-2 px-3 italic text-dark-3 transition delay-100 ease-in-out hover:border-primary-1 hover:bg-primary-1"
-            }
-          >
-            i
-          </button>
+        <div>
+          <div className="flex">
+            <h2 className="text-xl text-light-1">{props.name}</h2>
+            <button
+              type="button"
+              onClick={() => handleClick()}
+              className={
+                "ml-2 rounded-full border-primary-2 bg-primary-2 px-3 italic text-dark-3 transition delay-100 ease-in-out hover:border-primary-1 hover:bg-primary-1"
+              }
+            >
+              i
+            </button>
+          </div>
+          {message.length !== 0 && <p className="text-light-3">{message}</p>}
         </div>
         <div className="flex gap-80">
           <label>
@@ -39,15 +71,19 @@ function CWInput(props: any) {
               type="radio"
               name={props.name}
               value="show"
-              className="h-4 w-4"
+              className="h-6 w-6"
+              checked={visibility === "show"}
+              onChange={(e) => setVisibility(e.target.value)}
             />
           </label>
           <label>
             <input
               type="radio"
               name={props.name}
-              value="warn"
-              className="h-4 w-4"
+              value="flag"
+              className="h-6 w-6"
+              checked={visibility === "flag"}
+              onChange={(e) => setVisibility(e.target.value)}
             />
           </label>
           <label>
@@ -55,12 +91,13 @@ function CWInput(props: any) {
               type="radio"
               name={props.name}
               value="hide"
-              className="h-4 w-4"
+              className="h-6 w-6"
+              checked={visibility === "hide"}
+              onChange={(e) => setVisibility(e.target.value)}
             />
           </label>
         </div>
       </div>
-      {message.length !== 0 && <p className="text-light-3">{message}</p>}
     </div>
   );
 }
