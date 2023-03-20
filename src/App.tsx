a7df355c4aa3ff276ea98e5083cf176b094de21b
LightningStorms <70238209+LightningStorms@users.noreply.github.com>
2023-03-20 18:27:04 -0400

    Text Pages Added
    Text pages have been added in basic (and not necessarily pretty) HTML.
    Note: on attribution I had to use two <br> tags to get the spacing to look the same as the others, if there's another way to do that let me know.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import NotFound from "./pages/NotFound";
 import MovieEntry from "./pages/MovieEntry";
+import TosPage from "./pages/Tos";
+import AboutPage from "./pages/About";
+import AttributionPage from "./pages/Attribution";
+import PrivacyPage from "./pages/PrivacyPolicy";
 
 function App() {
   return (
@@ -18,6 +22,10 @@ function App() {
             <Route path="/search" element={<Search />} />
             <Route path="/movie/*" element={<MovieEntry />} />
             <Route path="/account/*" element={<Account />} />
+            <Route path="/tos" element={<TosPage />}/>
+            <Route path="/about" element={<AboutPage />}/>
+            <Route path="/attributions" element={<AttributionPage />}/>
+            <Route path="/privacypolicy" element={<PrivacyPage />}/>
             <Route path="/*" element={<NotFound />} />
           </Routes>
         </main>
