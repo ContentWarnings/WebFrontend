58b94dded601bb8e514b256ef617f22feddfdeb9
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2023-04-08 16:52:12 -0400

    Add a question mark icon for new users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,16 +4,29 @@
 import SearchBar from "../components/Home/SearchBar";
 import TrendingMovieSection from "../components/Home/TrendingMoviesSection";
 import GenresSection from "../components/Home/GenresSection";
+import { Tooltip } from "@mui/material";
 
 function Home() {
   return (
     <div className="relative mb-10 h-fit px-6">
       <div className="text-center">
         {/* Header */}
-        <h1 className="mt-48 text-6xl font-bold text-dark-3 dark:text-light-1 md:mt-32">
-          Find movies for
-          <span className="italic"> you</span>
-        </h1>
+        <div className="mt-48 flex items-end justify-center md:mt-32">
+          <h1 className="text-6xl font-bold text-dark-3 dark:text-light-1">
+            Find movies for
+            <span className="italic"> you</span>
+          </h1>
+          <Tooltip title="What is this?" placement="top">
+            <a
+              href="about/"
+              className={
+                "h-min rounded-full border border-transparent bg-primary-2 py-1 px-2 text-xs italic text-dark-3 transition delay-100 ease-in-out hover:bg-primary-1"
+              }
+            >
+              ?
+            </a>
+          </Tooltip>
+        </div>
         <SearchBar />
       </div>
       <TrendingMovieSection />
