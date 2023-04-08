7df42f7d40303e06f2eee56df204653282c17eb4
Shuga <git@shuga.co>
2023-04-08 17:58:20 -0400

    Fix a routing bug.

--- src/index.tsx
+++ src/index.tsx
@@ -18,15 +18,13 @@ let import_str: string = URLHelper.populateDefaultFromURL("in", "");
 if (import_str !== "") {
     Importer.import(import_str);
     Toast.toast("Content warning preferences imported successfully!");
-    if (window.location.pathname !== "/") {
-        window.location.reload();
-    }
 }
 
 // Initialize empty CW database.
 if (!localStorage.getItem("cw") || localStorage.getItem("cw") === "") {
     Importer.init();
-    if (window.location.pathname !== "/") {
+    let path = window.location.pathname.toLowerCase();
+    if (path !== "/" && path.indexOf("/account") === -1) {
         window.location.reload();
     }
 }
